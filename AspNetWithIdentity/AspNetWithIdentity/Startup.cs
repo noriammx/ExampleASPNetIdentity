05740ebe2ace623af050f60fc2e8c60f4bbc103b
noriam <marino.martinez@outlook.com>
2019-05-10 12:51:23 -0500

    Example code of the implementation of ASP Net Identity. Include custom database.

---
+++ AspNetWithIdentity/AspNetWithIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetWithIdentity.Startup))]
+namespace AspNetWithIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
